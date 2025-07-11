de71c618c818efc286829180226bf55cedb5cfd5
Lydia Neema <lydianeema25@gmail.com>
2025-07-11 10:19:57 +0300

    updtated

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+
+
+    function handleFocus(event){
+        console.log('Good!');
+
+    }
+    function handleBlur(event){
+        console.log('Hey! Eyes on me!');
+
+    }
+    
+
+return (
+<div>
+<button onFocus={handleFocus} onBlur={handleBlur}>EyesOnMe</button>
+<EyesOnMe/>
+</div>
+
+);
+
+}
+export default  EyesOnMe;
